4150494e1ec9680c9b4e7a8ca12aed601fc76949
Tomaš Monkevič <Toma Monkevi>
2017-03-31 15:39:13 +0300

    RIP Android support
    Former-commit-id: 59ae949689aaaa2fdd690b2c9175830f99375991

--- PrimeEngine/PrimeEngine/DllExport.h
+++ PrimeEngine/PrimeEngine/DllExport.h
@@ -1,14 +1,10 @@
 #ifndef DLL_EXPORT
 #define DLL_EXPORT
 
-#if PRIME_WINDOWS
-	#if _PRIME_ENGINE
-	#define PRIMEENGINEAPI __declspec(dllexport)
-	#else
-	#define PRIMEENGINEAPI __declspec(dllimport)
-	#endif
+#if _PRIME_ENGINE
+#define PRIMEENGINEAPI __declspec(dllexport)
 #else
-	#define PRIMEENGINEAPI
+#define PRIMEENGINEAPI __declspec(dllimport)
 #endif
 
 #endif // DLL_EXPORT
