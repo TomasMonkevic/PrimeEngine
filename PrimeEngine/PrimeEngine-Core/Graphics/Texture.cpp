ccc1475ceb73d3aeca2bff1b4e6211159d0f8ba4
Tomas Monkevic <tomas.monkevic97@gmail.com>
2017-05-30 23:22:04 +0300

    Image data is now deleted
    Former-commit-id: ed664ec238e5391c8866e7a3b8f8df0fc919a4b8

--- PrimeEngine/PrimeEngine-Core/Graphics/Texture.cpp
+++ PrimeEngine/PrimeEngine-Core/Graphics/Texture.cpp
@@ -20,19 +20,19 @@ namespace PrimeEngine { namespace Graphics {
 		GLuint result;
 		glGenTextures(1, &result);
 		glBindTexture(GL_TEXTURE_2D, result);
-		//unsigned pitch = 0;
+
 		BYTE* image = LoadImage(_path, &_width, &_height);
 		// Set the texture wrapping/filtering options (on the currently bound texture object)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		//GLenum colorType = pitch == 32 ? GL_BGRA : GL_BGR; //not sure if correct solution
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _width, 0, GL_BGRA, GL_UNSIGNED_BYTE, image); //doesn't work for every image format
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _width, 0, GL_BGRA, GL_UNSIGNED_BYTE, image);
 		glBindTexture(GL_TEXTURE_2D, 0);
 		glGenerateMipmap(GL_TEXTURE_2D);
-		//delete[] image;
-		//need to clean up image memory!!!!!!!!!!!!!
+
+		delete[] image;
 		return result;
 	}
 
--- PrimeEngine/PrimeEngine-Core/Utilities/ImageLoader.h
+++ PrimeEngine/PrimeEngine-Core/Utilities/ImageLoader.h
@@ -11,33 +11,45 @@ namespace PrimeEngine {
 		FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
 		FIBITMAP* dib = NULL;
 		fif = FreeImage_GetFileType(path, 0);
-		if (fif == FIF_UNKNOWN)
-			fif = FreeImage_GetFIFFromFilename(path);
-		if (fif == FIF_UNKNOWN)
-			return NULL;
-		if (FreeImage_FIFSupportsReading(fif))
-			dib = FreeImage_Load(fif, path);
-		if (!dib)
-			return NULL;
 
-		unsigned bitsPerPixel = FreeImage_GetBPP(dib);
+		if (fif == FIF_UNKNOWN)
+		{
+			fif = FreeImage_GetFIFFromFilename(path);
+		}
+		if (fif == FIF_UNKNOWN)
+		{
+			return NULL;
+		}
+		if (FreeImage_FIFSupportsReading(fif))
+		{
+			dib = FreeImage_Load(fif, path);
+		}
+		if (!dib)
+		{
+			return NULL;
+		}
+
 		//if not 32 bit depth -> convert to it
-		//is converting the best solution?
-		if (bitsPerPixel != 32)
+		if (FreeImage_GetBPP(dib) != 32)
 		{
 			FIBITMAP* tempImage = dib;
 			dib = FreeImage_ConvertTo32Bits(tempImage);
 		}
-		//----
+
 		*width = FreeImage_GetWidth(dib);
 		*height = FreeImage_GetHeight(dib);
 		BYTE* pixels = (BYTE*)FreeImage_GetBits(dib);
 
 		if ((pixels == 0) || (*width == 0) || (*height == 0))
+		{
 			return NULL;
-		//FreeImage_Unload(dib);
-		//clean up memory
-		return pixels;
+		}
+
+		BYTE* result = new BYTE[*width * *height * 4];
+		memcpy(result, pixels, *width * *height * 4);
+
+		FreeImage_Unload(dib);
+		return result;
 	}
 }
 
