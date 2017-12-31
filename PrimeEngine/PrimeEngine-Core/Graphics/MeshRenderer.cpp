7316ff81dfd0c203bd4a5a64c1d2b23df837ef9d
Tomas Monkevic <tomas.monkevic97@gmail.com>
2017-12-31 18:43:31 +0200

    Added specular lighting
    Former-commit-id: 459f25bf7dee080c02094573f53763517ed1c32c

--- PrimeEngine/PrimeEngine-Core/Graphics/MeshRenderer.cpp
+++ PrimeEngine/PrimeEngine-Core/Graphics/MeshRenderer.cpp
@@ -19,8 +19,10 @@ namespace PrimeEngine { namespace Graphics {
 
 		//TODO move this to material class
 		_material->SetUniform("light.position", Vector3(5.0f, 5.0f, 2.0f));
+		//_material->SetUniform("light.color", Vector4(1.0f, 0.0f, 0.0f, 1.0f)); //TODO this doesn't look good
 		_material->SetUniform("light.color", Vector4::one());
 		_material->SetUniform("light.intensity", 0.3f);
+		_material->SetUniform("viewPosition", camera.GetTransform().Position);
 		//-----------------------
 
 		_material->SetUniform("pr_matrix", camera.GetProjectionMatrix());
