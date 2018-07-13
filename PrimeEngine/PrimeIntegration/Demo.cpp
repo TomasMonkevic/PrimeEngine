4ec3861969ae40e81ebce2317811fea9c3acc36c
Tomas Monkevic <tomas.monkevic97@gmail.com>
2018-07-14 00:02:24 +0300

    Finished creating demo scene
    Former-commit-id: 3cb710de257e5ba5cf7c6e730280caffa1540756

--- PrimeEngine/PrimeIntegration/Demo.cpp
+++ PrimeEngine/PrimeIntegration/Demo.cpp
@@ -24,7 +24,7 @@ void Demo::Awake()
 	groundMaterial->DiffuseColor() = Color(0xFF317184);
 	ground->AddComponent(new MeshRenderer(Mesh::Cube(Color::white, 1000.0f, 0.1f, 1000.0f), groundMaterial));
 
-	AmbientLight* ambientLight = new AmbientLight(Color::white, 0.35f);
+	AmbientLight* ambientLight = new AmbientLight(Color(0xff9a9a9a), 0.75f);
 
 	sun = new DirectionalLight(Color(0xffd6f4ff), 1.0f);
 	//sun->GetTransform().Rotation = Quaternion(50.0f, -30.0f, 0.0f);
@@ -32,46 +32,81 @@ void Demo::Awake()
 	ModelImporter objModelImporter;
 
 	Material* floorMaterial = new Material(Shader::phong,
-		new Texture("Resources/SpecialScene/Textures/FloorCircular01_Albedo.png"));
-		//new Texture("Resources/SpecialScene/Textures/FloorCircular01_MetallicSmooth.png"));
-	//floorMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/FloorCircular01_Normal.png"));
+		new Texture("Resources/SpecialScene/Textures/FloorCircular01_Albedo.png"),
+		new Texture("Resources/SpecialScene/Textures/FloorCircular01_MetallicSmooth.png"));
+	floorMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/FloorCircular01_Normal.png"));
 	GameObject* floorCircular = new GameObject;
 	floorCircular->AddComponent(new MeshRenderer(objModelImporter.ImportModel("Resources/SpecialScene/Models/FloorCircular01.obj", true), floorMaterial));
-	floorCircular->GetTransform().Position = Vector3(-7.58f, 0.97f, -0.855f);
+	floorCircular->GetTransform().Position = Vector3(-6.58f, 0.97f, -2.855f);
 	floorCircular->GetTransform().Scale *= 0.01f;
 
 	Material* alienStatueMaterial = new Material(Shader::phong,
-		new Texture("Resources/SpecialScene/Textures/Statue_Albedo.png"));
-		//new Texture("Resources/SpecialScene/Textures/Statue_MetallicSmooth.png"));
-	//alienStatueMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/Statue_Normal.png"));
+		new Texture("Resources/SpecialScene/Textures/Statue_Albedo.png"),
+	new Texture("Resources/SpecialScene/Textures/Statue_MetallicSmooth.png"));
+	alienStatueMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/Statue_Normal.png"));
 	GameObject* alienStatue = new GameObject;
 	alienStatue->AddComponent(new MeshRenderer(objModelImporter.ImportModel("Resources/SpecialScene/Models/AlienStatue.obj", true), alienStatueMaterial));
-	alienStatue->GetTransform().Position = Vector3(-7.58f, 1.57f, -0.855f);
+	alienStatue->GetTransform().Position = Vector3(-6.58f, 1.0f, -2.855f);
 	alienStatue->GetTransform().Rotation = Quaternion(0.0f, 102.2f, 0.0f);
 	alienStatue->GetTransform().Scale *= 0.1f;
 
-	//GameObject* gates = GameObject::LoadObjModel("Resources/SpecialScene/Models/GateHuge01.obj", true);
-	//gates->GetTransform().Position = Vector3(17.97f, 0.13f, -2.855f);
-	//gates->GetTransform().Rotation = Quaternion(0.0f, -82.3f, 0.0f);
-	//gates->GetTransform().Scale *= 0.2f;
+	Material* gatesMaterial = new Material(Shader::phong,
+		new Texture("Resources/SpecialScene/Textures/GateHuge01_Albedo.png"),
+		new Texture("Resources/SpecialScene/Textures/GateHuge01_MetallicSmooth.png"));
+	gatesMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/GateHuge01_Normal.png"));
+	GameObject* gates = new GameObject;
+	gates->AddComponent(new MeshRenderer(objModelImporter.ImportModel("Resources/SpecialScene/Models/GateHuge01.obj", true), gatesMaterial));
+	gates->GetTransform().Position = Vector3(17.97f, 0.13f, -2.855f);
+	gates->GetTransform().Rotation = Quaternion(0.0f, -82.3f, 0.0f);
+	gates->GetTransform().Scale *= 0.2f;
 
-	//GameObject* rock01 = GameObject::LoadObjModel("Resources/SpecialScene/Models/RockChunk01.obj", true);
-	//rock01->GetTransform().Position = Vector3(8.84f, 1.68f, 21.47f);
-	//rock01->GetTransform().Rotation = Quaternion(0.0f, -51.933f, 0.0f);
-	//rock01->GetTransform().Scale *= 0.1f;
+	Mesh* rockMesh = objModelImporter.ImportModel("Resources/SpecialScene/Models/RockChunk01.obj", true);
+	Material* rocksMaterial = new Material(Shader::phong,
+		new Texture("Resources/SpecialScene/Textures/RockChunk01_Albedo.png"),
+		new Texture("Resources/SpecialScene/Textures/RockChunk01_MetallicSmooth.png"));
+	rocksMaterial->NormalMap(new Texture("Resources/SpecialScene/Textures/RockChunk01_Normal.png"));
+	GameObject* rock01 = new GameObject;
+	rock01->AddComponent(new MeshRenderer(rockMesh, rocksMaterial));
+	rock01->GetTransform().Position = Vector3(8.84f, 0.0f, 15.47f);
+	rock01->GetTransform().Rotation = Quaternion(0.0f, -139.782f, 0.0f);
+	rock01->GetTransform().Scale *= 0.15f;
+	GameObject* rock02 = new GameObject;
+	rock02->AddComponent(new MeshRenderer(rockMesh, rocksMaterial));
+	rock02->GetTransform().Position = Vector3(-28.8458f, 0.0f, 0.1698152f);
+	rock02->GetTransform().Rotation = Quaternion(0.0f, -70.933f, 0.0f);
+	rock02->GetTransform().Scale *= 0.15f;
+	GameObject* rock03 = new GameObject;
+	rock03->AddComponent(new MeshRenderer(rockMesh, rocksMaterial));
+	rock03->GetTransform().Position = Vector3(-13.44f, 0.0f, 18.28f);
+	rock03->GetTransform().Rotation = Quaternion(0.0f, -13.0f, 0.0f);
+	rock03->GetTransform().Scale *= 0.15f;
+	GameObject* rock04 = new GameObject;
+	rock04->AddComponent(new MeshRenderer(rockMesh, rocksMaterial));
+	rock04->GetTransform().Position = Vector3(-19.38909f, 0.0f, -22.1042);
+	rock04->GetTransform().Rotation = Quaternion(0.0f, -130.933f, 0.0f);
+	rock04->GetTransform().Scale *= 0.15f;
+	GameObject* rock05 = new GameObject;
+	rock05->AddComponent(new MeshRenderer(rockMesh, rocksMaterial));
+	rock05->GetTransform().Position = Vector3(5.374193, 0.0f, -22.91925);
+	rock05->GetTransform().Rotation = Quaternion(0.0f, -30.933f, 0.0f);
+	rock05->GetTransform().Scale *= 0.15f;
 
 	flashLight = new SpotLight(Color(0xffbafbfc), 0.7f, 15.0f, 50.0f, 15.0f);
 
 	mainScene = new Scene(mainCamera);
-	//mainScene->Add(rock01);
+	mainScene->Add(rock01);
+	mainScene->Add(rock02);
+	mainScene->Add(rock03);
+	mainScene->Add(rock04);
+	mainScene->Add(rock05);
 	mainScene->Add(alienStatue);
-	//mainScene->Add(gates);
+	mainScene->Add(gates);
 	mainScene->Add(ground);
 	mainScene->Add(floorCircular);
 	mainScene->Add(sun);
 	mainScene->Add(ambientLight);
 
-	mainScene->Add(flashLight);
+	//mainScene->Add(flashLight);
 }
 
 void Demo::Update()
