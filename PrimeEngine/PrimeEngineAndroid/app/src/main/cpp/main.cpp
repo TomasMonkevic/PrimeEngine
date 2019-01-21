5fd5ba20abb45ec5e12b67065e612f588f5f4912
Tomas Monkevic <tomas.monkevic97@gmail.com>
2019-01-22 00:09:16 +0200

    Fixed depth buffer
    Former-commit-id: 79d4e98227b2714586de0a8bf0707e8708f40f0b

--- PrimeEngine/PrimeEngineAndroid/app/src/main/cpp/main.cpp
+++ PrimeEngine/PrimeEngineAndroid/app/src/main/cpp/main.cpp
@@ -77,10 +77,12 @@ static int engine_init_display(struct engine* engine) {
      * component compatible with on-screen windows
      */
     const EGLint attribs[] = {
+            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, // request OpenGL ES 2.0
             EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
             EGL_BLUE_SIZE, 8,
             EGL_GREEN_SIZE, 8,
             EGL_RED_SIZE, 8,
+            EGL_DEPTH_SIZE, 16,
             EGL_NONE
     };
     EGLint w, h, format;
